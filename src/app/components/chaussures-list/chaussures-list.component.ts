cd5c34646b2028609cb56ef55af645821ce82f23
Augustin <Khelen578@live.fr>
2020-02-19 17:29:58 +0100

    go

--- src/app/components/chaussures-list/chaussures-list.component.ts
+++ src/app/components/chaussures-list/chaussures-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Chaussure } from 'src/app/models/chaussure';
 import { ChaussureService } from 'src/app/services/chaussure.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { SearchResult } from 'src/app/models/search-result';
 
 @Component({
   selector: 'app-chaussures-list',
@@ -13,15 +14,34 @@ export class ChaussuresListComponent implements OnInit {
 
   chaussures: Chaussure[];
   isLoading: boolean;
+  renardLoading: boolean;
+  renard: SearchResult;
 
   constructor(private chaussureService: ChaussureService, private modalService: BsModalService) { }
 
   ngOnInit() {
+    this.renardLoading = true;
     this.isLoading = true;
     this.chaussureService.getChaussures().subscribe((data: Chaussure[]) => {
       this.chaussures = data;
       this.isLoading = false;
     });
+    this.chaussureService.getRenard().subscribe((data: SearchResult) => {
+      this.renard = data;
+      this.renardLoading = false;
+    });
+    this.refreshRenard();
+  }
+
+  refreshRenard() {
+    setInterval(function () {
+      this.renardLoading = true;
+      this.chaussureService.getRenard().subscribe((data: SearchResult) => {
+        this.renard = data;
+        console.log('hello');
+        this.renardLoading = false;
+      });
+    }, 1000);
   }
 
   onDelete($event) {
--- src/app/services/chaussure.service.ts
+++ src/app/services/chaussure.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Chaussure } from '../models/chaussure';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
+import { catchError, retry } from 'rxjs/internal/operators';
+import { SearchResult } from '../models/search-result';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,7 @@ export class ChaussureService {
   listTypes = ['Sport', 'Ville', 'Football'];
   listMarques = ['Adidas', 'Nike', 'Puma'];
   apiUrl = "http://localhost:3000/chaussure";
+  foxApi = "http://93.29.84.137:3500/renard";
 
   constructor(private http: HttpClient, private toastr: ToastrService) { }
 
@@ -33,37 +36,58 @@ export class ChaussureService {
     return this.listMarques;
   }
 
+  getRenard(): Observable<SearchResult> {
+    return this.http.get<SearchResult>(this.foxApi).pipe(
+
+    );
+  }
+
   getChaussures(): Observable<Chaussure[]> {
     return this.http.get<Chaussure[]>(this.apiUrl).pipe(
-
+      retry(1),
+      catchError(this.handlError)
     );
   }
 
   getChaussureById(id: number): Observable<Chaussure> {
     return this.http.get<Chaussure>(this.apiUrl + '/' + id).pipe(
-
+      retry(1),
+      catchError(this.handlError)
     );
   }
 
   addChaussure(chaussure: Chaussure): Observable<Chaussure> {
     return this.http.post<Chaussure>(this.apiUrl, chaussure).pipe(
-
+      catchError(this.handlError)
     );
   }
 
   editChaussure(chaussure: Chaussure): Observable<Chaussure> {
     return this.http.put<Chaussure>(this.apiUrl + '/' + chaussure.id, chaussure).pipe(
-
+      retry(1),
+      catchError(this.handlError)
     );
   }
 
   deleteChaussure(id: number): Observable<Chaussure> {
     return this.http.delete<Chaussure>(this.apiUrl + '/' + id).pipe(
-
+      retry(1),
+      catchError(this.handlError)
     );
   }
 
-
+  handlError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
 
 
